7ff021062979401726110280379028e8877793f5
nbonvin <git@niin.org>
2016-06-09 11:41:14 +0200

    do not add NaN or infinite values (not supported by InfluxDB)

--- src/main/java/metrics_influxdb/measurements/Measure.java
+++ src/main/java/metrics_influxdb/measurements/Measure.java
@@ -196,7 +196,9 @@ public class Measure implements Measurement {
 		return addValue(key, Double.valueOf(value));
 	}
 	public Measure addValue(String key, double value) {
-		values.put(key, ""+value);
+		if (!((Double.isNaN(value)) || Double.isInfinite(value))) {
+			values.put(key, "" + value);
+		}
 		return this;
 	}
 	public Measure addValue(String key, int value) {

b580b15e24ddd59461c49efa26e14a642c2fe9f7
Jamie Grier <jamie.grier@gmail.com>
2016-08-28 23:19:53 -0700

    Using offer() instead of add(). add() throws exception if/when the queue is full.  The code expects it to return false.  This is what offer() does.

--- src/main/java/metrics_influxdb/misc/BoundedFIFO.java
+++ src/main/java/metrics_influxdb/misc/BoundedFIFO.java
@@ -11,14 +11,14 @@ public class BoundedFIFO<T> implements Queue<T> {
 
 	public BoundedFIFO(int capacity) {
 		this.delegate = new LinkedBlockingQueue<>(capacity);
-	} 
+	}
 
 	public int hashCode() {
 		return delegate.hashCode();
 	}
 
 	public boolean add(T e) {
-		while (!delegate.add(e)) {
+		while (!delegate.offer(e)) {
 			delegate.poll();
 		}
 		return true;

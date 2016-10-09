cd3d47b56e44496278aa6df71f78c4ab64ed2d8f
David Bernard <d.bernard1@ssi.samsung.com>
2016-10-09 19:00:55 +0200

    add option to specify “https” as scheme FIX #1

--- src/main/java/metrics_influxdb/v08/InfluxdbHttp.java
+++ src/main/java/metrics_influxdb/v08/InfluxdbHttp.java
@@ -15,6 +15,7 @@ package metrics_influxdb.v08;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
@@ -65,7 +66,7 @@ public class InfluxdbHttp implements Influxdb {
 	 * @throws IOException If the URL is malformed
 	 */
 	public InfluxdbHttp(String host, int port, String database, String username, String password) throws Exception  {
-		this(host, port, "", database, username, password, TimeUnit.MILLISECONDS);
+		this("http", host, port, "", database, username, password, TimeUnit.MILLISECONDS);
 	}
 
 	/**
@@ -73,7 +74,7 @@ public class InfluxdbHttp implements Influxdb {
 	 * @throws IOException If the URL is malformed
 	 */
 	public InfluxdbHttp(String host, int port, String path, String database, String username, String password) throws Exception  {
-		this(host, port, path, database, username, password, TimeUnit.MILLISECONDS);
+		this("http", host, port, path, database, username, password, TimeUnit.MILLISECONDS);
 	}
 
 	/**
@@ -83,7 +84,7 @@ public class InfluxdbHttp implements Influxdb {
 	 * @throws IOException If the URL is malformed
 	 */
 	public InfluxdbHttp(String host, int port, String database, String username, String password, TimeUnit timePrecision) throws Exception  {
-		this(host, port, "", database, username, password, timePrecision);
+		this("http", host, port, "", database, username, password, timePrecision);
 	}
 
 	/**
@@ -92,8 +93,8 @@ public class InfluxdbHttp implements Influxdb {
 	 *                      that does not return milliseconds epoch time for getTime()
 	 * @throws IOException If the URL is malformed
 	 */
-	public InfluxdbHttp(String host, int port, String path, String database, String username, String password, TimeUnit timePrecision) throws Exception  {
-		this.url = new URL("http", host, port,
+	public InfluxdbHttp(String scheme, String host, int port, String path, String database, String username, String password, TimeUnit timePrecision) throws Exception  {
+		this.url = new URL(scheme, host, port,
 				path + "/db/" + database
 				+ "/series?u=" + URLEncoder.encode(username, UTF_8.name())
 				+ "&p=" + password

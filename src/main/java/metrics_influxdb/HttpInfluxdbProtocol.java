2428dfed0dd5a1abfcb5b08eac4c47a39f7798db
Chinmay Nerurkar <cnerurkar@appnexus.com>
2018-02-26 13:03:58 -0500

    Make HTTP connect timeout and read timeout configurable

--- src/main/java/metrics_influxdb/HttpInfluxdbProtocol.java
+++ src/main/java/metrics_influxdb/HttpInfluxdbProtocol.java
@@ -4,7 +4,9 @@ public class HttpInfluxdbProtocol implements InfluxdbProtocol {
 	public final static String DEFAULT_HOST = "127.0.0.1";
 	public final static int DEFAULT_PORT = 8086;
 	public final static String DEFAULT_DATABASE = "metrics";
-
+    public final static long DEFAULT_CONNECT_TIMEOUT_SECONDS = 2;
+    public final static long DEFAULT_READ_TIMEOUT_SECONDS = 2;
+    
 	public final String scheme;
 	public final String user;
 	public final String password;
@@ -12,8 +14,10 @@ public class HttpInfluxdbProtocol implements InfluxdbProtocol {
 	public final int port;
 	public final boolean secured;
 	public final String database;
+	public final long connectTimeout;
+	public final long readTimeout;
 
-	public HttpInfluxdbProtocol(String scheme, String host, int port, String user, String password, String db) {
+	public HttpInfluxdbProtocol(String scheme, String host, int port, String user, String password, String db, long connectTimeout, long readTimeout) {
 		super();
 		this.scheme = scheme;
 		this.host = host;
@@ -22,6 +26,12 @@ public class HttpInfluxdbProtocol implements InfluxdbProtocol {
 		this.password = password;
 		this.database = db;
 		this.secured = (user != null) && (password != null);
+		this.connectTimeout = connectTimeout;
+		this.readTimeout = readTimeout;
+	}
+	
+	public HttpInfluxdbProtocol(String scheme, String host, int port, String user, String password, String db) {
+	    this(scheme, host, port, user, password, db, DEFAULT_CONNECT_TIMEOUT_SECONDS, DEFAULT_READ_TIMEOUT_SECONDS);
 	}
 	
 	public HttpInfluxdbProtocol(String host, int port, String user, String password, String db) {
